2c38bef996d17eb850ec4dae3defb1bbfec94192
fardoush <fardoushjahanara@gmail.com>
2023-03-27 19:47:12 +0600

    header menu create

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css';
+
+import React from 'react';
+import logo from '../../assets/images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className="header">
+            <img src={logo} alt="" />
+            
+            <div className="">
+            <a href="/shop">Shop</a>
+            <a href="/order">order</a>
+            <a href="/inventory">Inventory</a>
+            <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
